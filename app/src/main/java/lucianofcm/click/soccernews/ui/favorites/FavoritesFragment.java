3683ec51155ffa912111a383dd2a8ade49b5cc59
lucianofcm <lucweb@gmail.com>
2022-07-12 23:40:54 -0300

    Correção de bugs.

--- app/src/main/java/lucianofcm/click/soccernews/ui/favorites/FavoritesFragment.java
+++ app/src/main/java/lucianofcm/click/soccernews/ui/favorites/FavoritesFragment.java
@@ -4,30 +4,41 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import lucianofcm.click.soccernews.databinding.FragmentFavoritesBinding;
+import lucianofcm.click.soccernews.ui.adapter.NewsAdapter;
 
-
+/**
+ * A fragment representing a list of Items.
+ */
 public class FavoritesFragment extends Fragment {
 
     private FragmentFavoritesBinding binding;
+    private FavoritesViewModel favoritesViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        FavoritesViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(FavoritesViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        favoritesViewModel = new ViewModelProvider(this).get(FavoritesViewModel.class);
 
         binding = FragmentFavoritesBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        loadFavoriteNews();
+
+        return binding.getRoot();
+    }
+
+    private void loadFavoriteNews() {
+        favoritesViewModel.loadFavoriteNews().observe(getViewLifecycleOwner(), localNews -> {
+            binding.rvNews.setLayoutManager(new LinearLayoutManager(getContext()));
+            binding.rvNews.setAdapter(new NewsAdapter(localNews, updatedNews -> {
+                favoritesViewModel.saveNews(updatedNews);
+                loadFavoriteNews();
+            }));
+        });
     }
 
     @Override
